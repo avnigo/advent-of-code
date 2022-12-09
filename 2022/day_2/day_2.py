df51ee6868f7e0b980b2bc1d9c68cdb75b0a763f
avnigo <43263288+avnigo@users.noreply.github.com>
2022-12-09 18:19:20 +0200

    2022: Day 2 - Part Two

--- 2022/day_2/day_2.py
+++ 2022/day_2/day_2.py
@@ -2,17 +2,20 @@ from dataclasses import dataclass
 from enum import Enum
 from functools import total_ordering
 from itertools import cycle
+from typing import List, Optional
+from pathlib import Path
 
 
 @total_ordering
 class Choice(Enum):
-    ROCK = ("SCISSORS", 1)
-    PAPER = ("ROCK", 2)
-    SCISSORS = ("PAPER", 3)
+    ROCK = (1, "SCISSORS", "PAPER")
+    PAPER = (2, "ROCK", "SCISSORS")
+    SCISSORS = (3, "PAPER", "ROCK")
 
-    def __init__(self, beats, points):
-        self.beats = beats
+    def __init__(self, points, beats, loses):
         self.points = points
+        self.beats = beats
+        self.loses_to = loses
 
     def __gt__(self, other):
         return self.beats == other.name
@@ -26,13 +29,14 @@ class Result(Enum):
     
 @dataclass
 class RockPaperScissorsGame:
-    human: Choice
     elf: Choice
+    human: Optional[Choice] = None
+    result: Optional[Result] = None
 
     def __post_init__(self):
-        self.result = self.battle()
+        self.human = self.choice_from_result() if not self.human else self.human
+        self.result = self.battle() if not self.result else self.result
         self.points = self.result.value + self.human.points
-
 
     def battle(self) -> Result:
         return (
@@ -41,21 +45,63 @@ class RockPaperScissorsGame:
             else Result.LOSS
         )
 
+    def choice_from_result(self) -> Choice:
+        return (
+            Choice[self.elf.loses_to] if self.result == Result.WIN
+            else self.elf if self.result == Result.DRAW
+            else Choice[self.elf.beats]
+        )
 
-def part_one() -> int:
-    cipher = {play: choice for play, choice in zip("ABCXYZ", cycle(Choice))}
 
-    with open("./input") as file:
-        plays = [
+def part_one(puzzle_input: List[tuple], cipher: dict[str, Choice]) -> int:
+    return sum(
+        play.points for play in [
             RockPaperScissorsGame(
-                elf=cipher[hands[0]],
-                human=cipher[hands[1]],
-            ) for line in file if (hands := line.strip().split(" "))
+                elf=cipher[elf],
+                human=cipher[human],
+            ) for elf, human in puzzle_input
         ]
+    )
 
-    return sum(p.points for p in plays)
+
+def part_two(
+        puzzle_input: List[tuple],
+        play_cipher: dict[str, Choice],
+        result_cipher: dict[str, Result]
+    ) -> int:
+    return sum(
+        play.points for play in [
+            RockPaperScissorsGame(
+                elf=play_cipher[elf],
+                result=result_cipher[result],
+            ) for elf, result in puzzle_input
+        ]
+    )
+
+
+def decode_cipher(letters: str, map_to: Choice|Result) -> dict[str, Choice|Result]:
+    return {play: choice for play, choice in zip(letters, cycle(map_to))}  # type: ignore # issue: python/mypy#2305
+
+
+def read_input(file: Path) -> List[tuple]:
+    with open(file) as f:
+        return [tuple(hands) for line in f if (hands := line.strip().split(" "))]
 
 
 if __name__ == "__main__":
-    points = part_one()
-    print(points)
+    puzzle_input = read_input(Path("./input"))
+
+    points_1 = part_one(
+        puzzle_input,
+        cipher=decode_cipher("ABCXYZ", Choice)  # type: ignore
+    )
+
+    points_2 = part_two(
+        puzzle_input,
+        play_cipher=decode_cipher("ABC", Choice),  # type: ignore
+        result_cipher=decode_cipher("XYZ", Result),  # type: ignore
+    )
+
+    print("Part One:", points_1)
+    print("Part Two:", points_2)
+    
--- 2022/day_2/puzzle.md
+++ 2022/day_2/puzzle.md
@@ -1,5 +1,4 @@
 \--- Day 2: Rock Paper Scissors ---
------------------------------------
 
 The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant [Rock Paper Scissors](https://en.wikipedia.org/wiki/Rock_paper_scissors) tournament is already in progress.
 
@@ -18,14 +17,27 @@ For example, suppose you were given the following strategy guide:
     A Y
     B X
     C Z
-    
 
 This strategy guide predicts and recommends the following:
 
-*   In the first round, your opponent will choose Rock (`A`), and you should choose Paper (`Y`). This ends in a win for you with a score of _8_ (2 because you chose Paper + 6 because you won).
-*   In the second round, your opponent will choose Paper (`B`), and you should choose Rock (`X`). This ends in a loss for you with a score of _1_ (1 + 0).
-*   The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = _6_.
+- In the first round, your opponent will choose Rock (`A`), and you should choose Paper (`Y`). This ends in a win for you with a score of _8_ (2 because you chose Paper + 6 because you won).
+- In the second round, your opponent will choose Paper (`B`), and you should choose Rock (`X`). This ends in a loss for you with a score of _1_ (1 + 0).
+- The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = _6_.
 
 In this example, if you were to follow the strategy guide, you would get a total score of `_15_` (8 + 1 + 6).
 
 _What would your total score be if everything goes exactly according to your strategy guide?_
+
+\--- Part Two ---
+
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: `X` means you need to lose, `Y` means you need to end the round in a draw, and `Z` means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+- In the first round, your opponent will choose Rock (`A`), and you need the round to end in a draw (`Y`), so you also choose Rock. This gives you a score of 1 + 3 = _4_.
+- In the second round, your opponent will choose Paper (`B`), and you choose Rock so you lose (`X`) with a score of 1 + 0 = _1_.
+- In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = _7_.
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of `_12_`.
+
+Following the Elf's instructions for the second column, _what would your total score be if everything goes exactly according to your strategy guide?_
