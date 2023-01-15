419a09c7132b29bcd35982c5fa3d8b4ccc05a141
avnigo <43263288+avnigo@users.noreply.github.com>
2023-01-15 19:10:18 +0200

    Day 4 solved

--- 2022/day_4/day_4.py
+++ 2022/day_4/day_4.py
@@ -9,8 +9,16 @@ def str_to_range(*ranges: str) -> Generator:
     )
 
 
+def process_input(puzzle_input: list) -> Generator:
+    return (str_to_range(*pair.split(",")) for pair in puzzle_input)
+
+
 def check_subsets(x, y) -> bool:
     return x.issubset(y) or y.issubset(x)
+
+
+def check_intersect(*pairs: str) -> bool:
+    return any(set.intersection(*(set(c) for c in pairs)))
 
 
 def make_sets(pair: Generator) -> dict:
@@ -18,12 +26,14 @@ def make_sets(pair: Generator) -> dict:
 
 
 def part_one(puzzle_input: list) -> Generator:
-    return (
-        check_subsets(**make_sets(pair))
-        for pair in (str_to_range(*pair.split(",")) for pair in puzzle_input)
-    )
+    return (check_subsets(**make_sets(pair)) for pair in process_input(puzzle_input))
+
+
+def part_two(puzzle_input: list) -> Generator:
+    return (check_intersect(*pair) for pair in process_input(puzzle_input))
 
 
 if __name__ == "__main__":
     puzzle_input = Path("./input").read_text().splitlines()
-    print(sum(part_one(puzzle_input)))
+    print("Part One:", sum(part_one(puzzle_input)))
+    print("Part Two:", sum(part_two(puzzle_input)))
